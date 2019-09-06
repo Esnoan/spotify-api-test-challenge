6f897c30d2d90630d1d689f1886d9c371ad1abfe
Andrés U <andrewu121@gmail.com>
2019-09-06 15:20:38 -0500

    Project setup

---
+++ test/SpotifyApiTest.js
@@ -0,0 +1,9 @@
+const { assert } = require('chai');
+
+describe('Spotify Api Test', () => {
+  describe('Empty test', () => {
+    it('Project setup', () => {
+      assert.equal(1, 1);
+    });
+  });
+});
