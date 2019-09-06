7caa25ac2e9dda2416cbaccb6e524bd7067d2e4d
Andrés U <andrewu121@gmail.com>
2019-09-06 18:12:25 -0500

    Auth spotify test

--- test/SpotifyApiTest.js
+++ test/SpotifyApiTest.js
@@ -1,9 +1,20 @@
-const { assert } = require('chai');
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const chai = require('chai');
+
+const { expect } = chai;
 
 describe('Spotify Api Test', () => {
-  describe('Empty test', () => {
-    it('Project setup', () => {
-      assert.equal(1, 1);
-    });
+  it('Auth test', async () => {
+    // Client Credentials Flow
+    const auth = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64');
+    const response = await agent
+      .post('https://accounts.spotify.com/api/token')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Authorization', `Basic ${auth}`)
+      .send('grant_type=client_credentials');
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('access_token');
   });
 });
