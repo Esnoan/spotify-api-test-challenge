2050ebe643e46649d807d0a65508db36ba5be809
Andrés U <andrewu121@gmail.com>
2019-09-06 21:20:19 -0500

    search test

--- test/SpotifyApiTest.js
+++ test/SpotifyApiTest.js
@@ -4,16 +4,36 @@ const chai = require('chai');
 
 const { expect } = chai;
 
+async function getToken() {
+  const auth = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64');
+  const response = await agent
+    .post('https://accounts.spotify.com/api/token')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .set('Authorization', `Basic ${auth}`)
+    .send('grant_type=client_credentials');
+  return response;
+}
+
 describe('Spotify Api Test', () => {
   it('Auth test', async () => {
-    const auth = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64');
-    const response = await agent
-      .post('https://accounts.spotify.com/api/token')
-      .set('Content-Type', 'application/x-www-form-urlencoded')
-      .set('Authorization', `Basic ${auth}`)
-      .send('grant_type=client_credentials');
-
+    const response = await getToken();
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body).to.have.property('access_token');
+  });
+
+  it('Search test', async () => {
+    const query = {
+      q: 'Till I Collapse',
+      type: 'track'
+    };
+
+    const getAuth = await getToken();
+    const response = await agent
+      .get('https://api.spotify.com/v1/search')
+      .set('Authorization', `Bearer ${getAuth.body.access_token}`)
+      .query(query);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.tracks.items.length).to.equal(20);
   });
 });
